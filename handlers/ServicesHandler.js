32d3321ba5b003848ae98adf7cf07806cd0352cb
Edi Bălan <142690701+edibalan@users.noreply.github.com>
2023-09-29 23:25:37 +0300

    Update ServicesHandler.js

--- handlers/ServicesHandler.js
+++ handlers/ServicesHandler.js
@@ -1,12 +1,9 @@
 "use strict";
 import { QSAll } from "https://edibalan.github.io/alice-kineto/app.js";
 import Services from "https://edibalan.github.io/alice-kineto/components/Services.js";
-
 export default class ServicesHandler extends Services {
   constructor() {
     super();
-    
-    this.screenWidth = screen.width;
     this.servicesButtons = QSAll(".service-button");
 
     this.extendDescription = event => {
@@ -16,21 +13,19 @@ export default class ServicesHandler extends Services {
       description.classList.add("extend-description");
 
       button.removeEventListener("click", this.extendDescription);
-      button.addEventListener("click", this.hideDescription);
+      button.addEventListener("click", this.shrinkDescription);
     };
     
-    this.hideDescription = event => {
+    this.shrinkDescription = event => {
       const button = event.target, description = event.target.previousElementSibling;
 
       button.innerText = "Mai mult";
       description.classList.remove("extend-description");
 
-      button.removeEventListener("click", this.hideDescription);
+      button.removeEventListener("click", this.shrinkDescription);
       button.addEventListener("click", this.extendDescription);
     };
   }
 
-  initiateHandler() {
-    this.servicesButtons.forEach(button => button.addEventListener("click", this.extendDescription));
-  }
+  initiate() {this.servicesButtons.forEach(button => button.addEventListener("click", this.extendDescription))}
 }
