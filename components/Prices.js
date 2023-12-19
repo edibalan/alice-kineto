a85b1a53aa4093b188657a7793c2def722c6c581
Edi Bălan <142690701+edibalan@users.noreply.github.com>
2023-12-19 23:01:01 +0200

    Update Prices.js

--- components/Prices.js
+++ components/Prices.js
@@ -1,55 +1,55 @@
 "use strict";
 export default class Prices {
   constructor(data) {
-    this.renderPricesList = length => {
-      let pricesList = "";
+    this.renderTableBody = length => {
+      const width = screen.width;
+      let pricesTable = "";
 
-      for(let i = 0; i < length; i++) {
-        pricesList += `
+      for (let i = 0; i < length; i++) {
+        pricesTable += `
           <tr>
-            <td class="prices-services">
-              ${window.innerWidth >= 460 ? [i + 1] + ". " + data["prices-services"][i] : data["prices-services"][i]}
+            <td class="prices__services">
+              ${width >= 460 ? i + 1 + ". " + data.prices.services[i] : data.prices.services[i]}
             </td>
-            <td class="prices-times">
-              ${window.innerWidth >= 460 ? data["prices-times"][i] + " minute" : data["prices-times"][i]}
+            <td class="prices__times">
+              ${width >= 460 ? data.prices.times[i] + " minute" : data.prices.times[i]}
             </td>
-            <td class="prices-costs">
-              ${window.innerWidth >= 460 ? data["prices-costs"][i] + " ron" : data["prices-costs"][i]}
+            <td class="prices__costs">
+              ${width >= 460 ? data.prices.costs[i] + " ron" : data.prices.costs[i]}
             </td>
           </tr>
         `;
-      };
-      
-      return pricesList;
+      }
+
+      return pricesTable;
     };
   }
-  
+
   render() {
     return `
-      <section class="prices-section">
-        <div class="container | text-center" id="prices">
-          <div class="section-title">
-            <h1 class="fs-large fw-bold">Preturi</h1>
-            <div class="title-underline"></div>
-          </div>
-          <table class="prices-list" border="1">
-            <colgroup>
-              <col>
-            </colgroup>
-            
-            <thead>
-              <tr class="fw-bold">
-                <th class="prices-header">Serviciu</th>
-                <th id="time">Timp</th>
-                <th id="price">Pret</th>
-              </tr>
-            </thead>
-            
-            <tbody class="prices-services-list">${this.renderPricesList(4)}</tbody>
-          </table>
-          <button class="prices-button | button fs-small fw-sm-bold" data-type="prices-button">Mai mult</button>
-        </div>
-      </section>
+      <article class="prices | container" id="prices">
+        <h2 class="prices__title">Preturi</h2>
+
+        <table class="prices__table" border="1">
+          <colgroup>
+            <col>
+          </colgroup>
+          
+          <thead>
+            <tr>
+              <th class="prices__table__header">Serviciu</th>
+              <th id="time">Timp</th>
+              <th id="price">Pret</th>
+            </tr>
+          </thead>
+          
+          <tbody class="prices__table__body">
+            ${this.renderTableBody(3)}
+          </tbody>
+        </table>
+
+        <button class="prices__button">Mai mult</button>
+      </article>
     `;
   }
 }
