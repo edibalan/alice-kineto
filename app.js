14fe9f6fef4ddec78ba9244dc7011d265edddf1b
Edi Bălan <142690701+edibalan@users.noreply.github.com>
2023-12-19 23:42:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ import Home from "https://edibalan.github.io/alice-kineto/components/Home.js";
 import Services from "https://edibalan.github.io/alice-kineto/components/Services.js";
 import Gallery from "https://edibalan.github.io/alice-kineto/components/Gallery.js";
 import Prices from "https://edibalan.github.io/alice-kineto/components/Prices.js";
-import Contact from "https://edibalan.github.io/alice-kineto/components/Footer.js";
+import Footer from "https://edibalan.github.io/alice-kineto/components/Footer.js";
 
 import NavigationHandler from "https://edibalan.github.io/alice-kineto/handlers/NavigationHandler.js";
 import ServicesHandler from "https://edibalan.github.io/alice-kineto/handlers/ServicesHandler.js";
 import GalleryHandler from "https://edibalan.github.io/alice-kineto/handlers/GalleryHandler.js";
 import PricesHandler from "https://edibalan.github.io/alice-kineto/handlers/PricesHandler.js";
 
-export const QS = (elem) => document.querySelector(elem), QSAll = (elem) => document.querySelectorAll(elem);
+export const QS = elem => document.querySelector(elem), QSAll = elem => document.querySelectorAll(elem);
 class App {
   constructor() {
     this.body = QS("body");
@@ -21,8 +21,7 @@ class App {
   }
 
   async render() {
-    const request = await fetch("https://edibalan.github.io/alice-kineto/data/data.json", { method: "GET" }),
-      data = await request.json();
+    const request = await fetch("https://edibalan.github.io/alice-kineto/data/data.json", { method: "GET" }), data = await request.json();
 
     try {
       if (request.status === 200) {
@@ -35,7 +34,7 @@ class App {
           ${new Prices(data).render()}
         `;
 
-        this.footer.innerHTML = new Contact().render();
+        this.footer.innerHTML = new Footer().render();
         this.footer.setAttribute("class", "footer");
 
         this.body.insertAdjacentElement("afterbegin", this.footer);
@@ -49,11 +48,11 @@ class App {
           new PricesHandler(data).initiate();
         }, 100);
       } else console.log(`Something went wrong. Status code: ${request.status}`);
-    } catch (error) {
+    } 
+     catch (error) {
       console.log(error);
     }
   }
 }
 
 new App().render();
-
